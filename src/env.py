a7b29fffbc0a362bffa27a690c261c1210537f44
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-29 02:24:05 +0200

    added support for global config in env.py

--- src/env.py
+++ src/env.py
@@ -5,10 +5,13 @@ import subprocess as sb
 import cap
 import quapy as qp
 
-_hostname = sb.run(["hostname"], capture_output=True).stdout.decode("UTF-8").strip()
-with open("env.json", "r") as f:
-    ext_env = json.load(f).get(_hostname, cap.env)
-cap.env |= ext_env
+if os.path.exists("env.json"):
+    _hostname = sb.run(["hostname"], capture_output=True).stdout.decode("UTF-8").strip()
+    with open("env.json", "r") as f:
+        _jdict = json.load(f)
+        _env = _hostname if _hostname in _jdict else "global"
+        ext_env = _jdict.get(_env, cap.env)
+    cap.env |= ext_env
 
 PROJECT = "tms"
 root_dir = os.path.join(cap.env["OUT_DIR"], PROJECT)
